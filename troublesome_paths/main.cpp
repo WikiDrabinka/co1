6b17edf001131cae03dc1074be328ef1910a5587
Dnimevih <wojdud.pl@gmail.com>
2024-11-03 14:56:32 +0100

    Created a get_coordinates function and started A*

--- troublesome_paths/main.cpp
+++ troublesome_paths/main.cpp
@@ -5,14 +5,23 @@ struct Arc {
   int weight;
   int dest;
 };
-
-std::vector<Arc>* read_nodes(int* number_of_nodes) {
+std::vector<Arc>* read_nodes(int* num_rows, int* num_cols, int* num_arcs) {
   // Return type: list of lists of arcs
   int rows, columns, arcs;
   std::vector<Arc>* res;
   std::cin >> rows >> columns >> arcs;
   int nodes = rows * columns;
-  *number_of_nodes = nodes;
+
+  /*
+    * M -> column
+    * N -> row
+    * A -> number of arcs
+    */
+
+  *num_rows = rows;
+  *num_cols = columns;
+  *num_arcs = arcs;
+
   res = new std::vector<Arc>[nodes];
   for(int i = 0; i < nodes; ++i) {
     res[i].clear();
@@ -27,15 +36,37 @@ std::vector<Arc>* read_nodes(int* number_of_nodes) {
     tmp.dest = to;
 
     res[from].push_back(tmp);
+    std::cout << from << "\n";
   }
 
   return res;
 }
 
+std::pair<int, int> get_coordinates(int index, int rows, int columns) {
+  std::pair<int, int> res;
+  // Order doesn't matter as long as it's the same for all nodes
+  res.second = index % columns;
+  res.first = (index - (index % columns)) / columns;
+  return res;
+}
+
+std::vector<int> astar(std::vector<Arc>* graph, int rows, int columns, int arcs, int from, int to) {
+  std::vector<int> res;
+  res.clear();
+}
+
 int main() {
 
-  int number_of_nodes = 0;
-  std::vector<Arc>* graph = read_nodes(&number_of_nodes);
+  int rows, columns, arcs;
+  std::vector<Arc>* graph = read_nodes(&rows, &columns, &arcs);
+
+  int start_point = 0;
+  int middle_point = rows - 1;
+  int end_point = middle_point * columns;
+
+  for(int i = 0; i < arcs; ++i) {
+    std::cout << get_coordinates(i, rows, columns).first << ", " << get_coordinates(i, rows, columns).second << "\n";
+  }
 
   delete graph;
   return 0;
